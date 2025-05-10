79a47c6c02a917d600c125c831efba0056b9eb12
Tharuksha <tharukshawickramarachchi@gmail.com>
2025-05-11 01:42:21 +0530

    Header/Hero/Search Frontend | Commit

---
+++ src/home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Category from "./components/Category";
+function Home() {
+  return (
+    <div>
+      {/*Header*/}
+      <Header />
+      {/*Hero*/}
+      <Hero />
+      {/*Categories*/}
+      <Category />
+    </div>
+  );
+}
+
+export default Home;
